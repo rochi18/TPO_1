aa566881b11d9ed1fe12e9f7c9a76814b39b2feb
Maria Alejandra Rodriguez <87164629+Marialer27@users.noreply.github.com>
2022-10-26 13:05:57 -0300

    Actualizo datos api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let url = 'http://localhost:3000/producto';
+let url = 'https://api.sampleapis.com/coffee/iced';
 
 async function reload(){
     const res = await fetch(url);
@@ -10,8 +10,8 @@ async function reload(){
     const img3 = document.getElementById('img3');
     const img4 = document.getElementById('img4');
     img1.src = data[0].path;
-    img2.src = data[1].path;  
-    img3.src = data[2].path;
+    img2.src = data[5].path;  
+    img3.src = data[1].path;
     img4.src = data[3].path;  
    }
 reload();
